e2180dd6354193142a878cb3e47f3849e5dbe915
pablot95 <pablo.travi95@gmail.com>
2024-10-17 19:20:17 -0300

    Landing

--- main.js
+++ main.js
@@ -13,16 +13,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
         if (scrollTop > lastScrollTop) {
             
-            header.style.top = "-60px"; 
+            header.style.top = "-200px"; 
         } else {
         
-            header.style.top = "-60px"; 
+            header.style.top = "-200px"; 
         }
 
         
         timeout = setTimeout(function() {
             header.style.top = "0"; 
-        }, 500); //
+        }, 300); //
 
         lastScrollTop = scrollTop; 
     });
@@ -155,58 +155,3 @@ menuToggle.addEventListener('click', function() {
 
 
 
-const carruselWrapper = document.getElementById('carruselWrapper');
-const carruselItems = document.querySelectorAll('.carruselitem');
-let startX; // Posición inicial del toque
-let currentTranslateX = 0; // Posición actual del desplazamiento
-let isDragging = false; // Estado de arrastre
-let totalItems = carruselItems.length / 2; // Cantidad de imágenes originales
-let currentIndex = 0; // Índice actual
-
-// Maneja el inicio del toque
-carruselWrapper.addEventListener('touchstart', (event) => {
-    isDragging = true;
-    startX = event.touches[0].clientX; // Guarda la posición inicial del toque
-});
-
-// Maneja el movimiento del toque
-carruselWrapper.addEventListener('touchmove', (event) => {
-    if (!isDragging) return;
-
-    const currentX = event.touches[0].clientX; // Posición actual del toque
-    const diffX = currentX - startX; // Diferencia entre la posición actual y la inicial
-
-    // Desplaza el carrusel
-    carruselWrapper.style.transform = `translateX(${currentTranslateX + diffX}px)`;
-});
-
-// Maneja el final del toque
-carruselWrapper.addEventListener('touchend', () => {
-    isDragging = false;
-
-    // Calcula la nueva posición al soltar el dedo
-    const currentX = parseFloat(carruselWrapper.style.transform.replace('translateX(', '').replace('px)', ''));
-    const threshold = 100; // Umbral para cambiar de imagen
-
-    // Determina si se debe cambiar a la siguiente o anterior imagen
-    if (currentX - startX < -threshold) {
-        // Siguiente imagen
-        currentIndex++;
-    } else if (currentX - startX > threshold) {
-        // Imagen anterior
-        currentIndex--;
-    }
-
-    // Limita el índice al rango de imágenes
-    if (currentIndex >= totalItems) {
-        currentIndex = 0; // Vuelve al inicio
-    } else if (currentIndex < 0) {
-        currentIndex = totalItems - 1; // Va al final
-    }
-
-    // Calcula la nueva posición de desplazamiento
-    currentTranslateX = -currentIndex * (70 + 15); // Ajusta según el ancho de los elementos
-
-    // Aplica la transformación final
-    carruselWrapper.style.transform = `translateX(${currentTranslateX}%)`;
-});
